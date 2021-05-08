571a9fba289717bcbb715416ab9d1d83c778a3ab
pwrmln <vvdnet@pm.me>
2021-05-08 19:34:54 +0100

    what

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.once('ready', async () => {
     // eslint-disable-next-line no-undef
+    const data = {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    };
+
+    const command = await client.guilds.cache.get('829728636113256470')?.commands.create(data);
+    console.log(command);
+    // eslint-disable-next-line no-undef
     require(`${__dirname}/webserver`)(client);
     client.chn = client.guilds.cache.get('829728636113256470').channels.cache.get('829728636563095623');
 });
